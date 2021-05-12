4dba5555f48033c075c2d4082f405b91f49aad6c
Axelle Clochard <axelle.cb@gmail.com>
2021-05-12 12:56:13 -0400

    Updated reactive text with proper labels

--- js/quiz.js
+++ js/quiz.js
@@ -16,15 +16,15 @@ document.getElementById("form1").onsubmit=function() {
    genre = document.querySelector('#form1 input[name = "genre"]:checked').value;
      
    // initialize score variables 
-   quiet = 0;
-   spoon = 0;
-   willie = 0;
+   science = 0;
+   oil = 0;
+   other = 0;
    
    //function to calculate score for each question
    function eachscore(x){
-   if(x == "quiet") { quiet = quiet + 1}
-   if(x == "spoon") { spoon = spoon + 1}
-   if(x == "willie") { willie = willie + 1}
+   if(x == "science") { science = science + 1}
+   if(x == "oil") { oil = oil + 1}
+   if(x == "other") { other = other + 1}
    }
    // for the above function, you could also change the 1 to a variable so you could give more points for certain questions
    
@@ -33,15 +33,19 @@ document.getElementById("form1").onsubmit=function() {
    eachscore(eval("activity"));
    eachscore(eval("genre"));
       
-   all = [quiet, spoon, willie];
+   all = [science, oil, other];
    
  //get the max score  in the array
  maxscore = Math.max.apply(Math,all);
  
  // object holding scores and feedback	
- scores = [{index:0, feedback: "Quiet Company"},
-           {index:1, feedback: "Spoon"},
-           {index:2, feedback: "Willie Nelson"}];
+ scores = [{index:0, feedback: "HONEST SCIENTIST"},
+           {index:1, feedback: "OIL TYCOON"},
+           {index:2, feedback: "OTHER"}];
+
+ scorez = [{index:0, feedback: "SAVE THE PLANET"},
+           {index:1, feedback: "DESTROY THE PLANET"},
+           {index:2, feedback: "SAVE THE UNITED PLATES"}];
  
  //figure out which index # holds the max score 
  for(i_prime=0; i_prime<all.length; i_prime++) {
@@ -49,7 +53,7 @@ document.getElementById("form1").onsubmit=function() {
  
     //this gets one answer, the last one it encounters with a match
  document.getElementById("answer").innerHTML = scores[i_prime].feedback;
- document.getElementById("answer2").innerHTML = scores[i_prime].feedback;
+ document.getElementById("answer2").innerHTML = scorez[i_prime].feedback;
 
  
  //this version would allow for appending multiple answers; replace statement above
