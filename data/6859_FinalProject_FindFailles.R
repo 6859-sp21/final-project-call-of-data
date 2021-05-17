6f9b3b874ecca1dc6a58eb2e78c4d62adc55572e
Axelle Clochard <axelle.cb@gmail.com>
2021-05-17 11:10:13 -0400

    Pushed reshaped dataset with 90s growth data

--- data/6859_FinalProject_FindFailles.R
+++ data/6859_FinalProject_FindFailles.R
@@ -117,6 +117,8 @@ country <- merge(x = country,
                  
                  by = "Location")
 
+
+
 # Re-Organize
 country <- country[c("Year", "Year_num", "Location", "Population", "Pop_Quartile","GDP", "GDP_Quartile", "Group1", "Group2", "Group3", "Group4", "Group5", 
                      "CO2", "CO2_per_GDP", "Share_of_Global_CO2", "CO2_per_Capita", "CO2_per_Unit_Energy", "Primary_Energy_Consumption", "Energy_per_Capita", "Energy_per_GDP", "Growth_CO2", "Growth_Primary_Energy", "Growth_Pop")]
@@ -125,11 +127,58 @@ country <- country[c("Year", "Year_num", "Location", "Population", "Pop_Quartile
 # Re-shape to match example:
 country <- gather(country, Metric, Value, CO2:Growth_Pop, factor_key=TRUE)
 
-# Write to CSV
-write.csv(country, "reshaped_country_data.csv", row.names = F)
 
 
-# #### B3. COMBINE AND WRITE TO CSV
-# data <- rbind(global, country)
-# write.csv(data, "reshaped_all_data.csv", row.names = F)
+#### B3. Read in COUNTRY-LEVEL DATA: 90s
+country90s = read.csv("./country-level-90s.csv", stringsAsFactors = F)
+
+# Drop Row Index column
+country90s <- subset(country90s, select = -c(X))
+
+# Simplify column names:
+names(country90s) <- c("Location", "Year", 
+                    "CO2", "CO2_per_GDP", "Share_of_Global_CO2", "CO2_per_Capita", "CO2_per_Unit_Energy", "Primary_Energy_Consumption", "Energy_per_Capita", "Energy_per_GDP", 
+                    "Population", "GDP", "Group1", "Group2", "Group3", "Group4", "Group5", 
+                    "Growth_CO2", "Growth_Primary_Energy", "Growth_Pop")
+
+# Convert Year to a Data
+country90s$Year_num <- country90s$Year 
+country90s$Year <- as.Date(ISOdate(country90s$Year, 1, 1)) 
+
+
+# Country Grouping Variables
+country_2016 <- subset(country90s, Year_num == 2016) %>%
+  mutate(GDP_Quartile = ntile(GDP, 4)) %>%
+  mutate(Pop_Quartile = ntile(Population, 4))
+
+country90s <- merge(x = country90s,
+                    y = subset(country90s, Year_num == 2016) %>%
+                        mutate(GDP_Quartile = ntile(GDP, 4),
+                              Pop_Quartile = ntile(Population, 4))%>%
+                        subset(., select = c(Location, GDP_Quartile, Pop_Quartile)),
+                 by = "Location")
+
+
+
+# Re-Organize
+country90s <- country90s[c("Year", "Year_num", "Location", "Population", "Pop_Quartile","GDP", "GDP_Quartile", "Group1", "Group2", "Group3", "Group4", "Group5", 
+                     "CO2", "CO2_per_GDP", "Share_of_Global_CO2", "CO2_per_Capita", "CO2_per_Unit_Energy", "Primary_Energy_Consumption", "Energy_per_Capita", "Energy_per_GDP", "Growth_CO2", "Growth_Primary_Energy", "Growth_Pop")]
+
+
+# Re-shape to match example:
+country90s <- gather(country90s, Metric, Value, CO2:Growth_Pop, factor_key=TRUE)
+
+
+# Add a 90s label to the metrics:
+country90s$Metric <- paste0(country90s$Metric, "_90s")
+
+
+
+
+#### B3. MERGE THE TWO DATA SETS AND WRITE TO CSV
+data <- rbind(x = country90s, 
+              y = country)
+
+
+write.csv(data, "reshaped_country_data.csv", row.names = F)
 
